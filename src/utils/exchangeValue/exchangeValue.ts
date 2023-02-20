5de39ac2a288e23e7fe9db0a6a4e123854e02494
RudVladyslav <gydinidog74@gmail.com>
2023-02-20 21:03:31 +0200

    first commit

---
+++ src/utils/exchangeValue/exchangeValue.ts
@@ -0,0 +1,45 @@
+import {IDataItem} from "../interfaces";
+
+interface IExchangeValue {
+    changeType: string
+    getType: string
+    changeValue: string
+    getValue: string
+    data: IDataItem[],
+    calculateType?: string
+}
+
+export const exchangeValue = ({
+                                  changeType,
+                                  changeValue,
+                                  getType,
+                                  getValue,
+                                  data,
+                                  calculateType = ''
+                              }: IExchangeValue): any => {
+
+    const workData = data.filter((item: any) =>
+        item.base_ccy === changeType
+        && item.ccy === getType
+        || item.ccy === changeType
+        && item.base_ccy === getType
+    )[0]
+    if (calculateType === 'change' || calculateType === '') {
+        if (workData.base_ccy === changeType) {
+            const updatedValue = (Number(changeValue) / Number(workData.buy)).toFixed(2)
+            return {value: updatedValue.toString(), type: calculateType}
+        } else {
+            const updatedValue = (Number(changeValue) * Number(workData.sale)).toFixed(2)
+            return {value: updatedValue.toString(), type: calculateType}
+        }
+    } else if (calculateType === 'get') {
+        if (workData.ccy === changeType) {
+            const updatedValue = (Number(getValue) / Number(workData.sale)).toFixed(2)
+            return {value: updatedValue.toString(), type: calculateType}
+        } else {
+            const updatedValue = Math.round(Number(getValue) * Number(workData.buy))
+            return {value: updatedValue.toString(), type: calculateType}
+        }
+
+    }
+}
